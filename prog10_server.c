ed40e0d8e19ecc2158ba77d210cb7c4c1989a64d
Tarun S <tarun.shiva.sujatha@gmail.com>
2016-08-24 22:12:47 +0530

    Update prog10_server.c

--- prog10_server.c
+++ prog10_server.c
@@ -7,14 +7,14 @@
     {
         char fname[50],buffer[1025];
         int req,res,n,file;
-        mkfifo("req.fifo",0777);/*creates a fifo file of permission 777*/
+        mkfifo("req.fifo",0777);
         mkfifo("res.fifo",0777);
         printf("waiting for request...\n");
-        req=open("req.fifo",O_RDONLY);/*opens a read-only file req.fifo and stores the file descriptor value in req*/
-        res=open("res.fifo",O_WRONLY);/*opens a write & read file res.fifo and stores the file descriptor value in res*/
-        read(req,fname,sizeof(fname));/* reads the content of the file req.fifo by referencing the file descriptor value req and stores it in fname array*/
+        req=open("req.fifo",O_RDONLY);
+        res=open("res.fifo",O_WRONLY);
+        read(req,fname,sizeof(fname));
         printf("received request for %s\n",fname);
-        file=open(fname,O_RDONLY);/*opens fname as Read-only*/
+        file=open(fname,O_RDONLY);
         if(file<0)
             write(res,"File not found\n",15);
         else
